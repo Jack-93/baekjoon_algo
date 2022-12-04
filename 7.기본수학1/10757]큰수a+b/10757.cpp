c5e3aa53ec8ca57d3b04dfb71382e3ed414996e7
OheJaeWook <worl5@korea.ac.kr>
2022-12-04 20:58:27 +0900

    2022.12.04

---
+++ 7.기본수학1/10757]큰수a+b/10757.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+// 첫째 줄에 A와 B가 주어진다.(0 < A, B < 1010000) int main()
+int main()
+{
+    long long A, B = 0;
+    cin >> A >> B;
+
+    return 0;
+}
--- 7.기본수학2/1978]소수찾기/1978.cpp
+++ 7.기본수학2/1978]소수찾기/1978.cpp
@@ -2,30 +2,30 @@
 #include <cmath>
 using namespace std;
 // 첫 줄에 수의 개수 N이 주어진다.N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
-// 1 != prime
+// 에라스토테네스의 체
 int main()
 {
     int n = 0;
+    bool prime[n + 1];
     int count = 0;
     cin >> n;
-    int num = 0;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 2; i < n; i++)
     {
-        cin >> num;
-        for (int j = 2; j < num; j++)
-        {
-            if (num % j == 0)
-            {
-                break;
-            }
-            else
-            {
-                count++;
-                break;
-            }
-        }
+        prime[i] = true;
     }
 
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (prime[i] == true)
+            for (int j = i * i; j <= n; j += i)
+            {
+                prime[j] = false;
+                if (prime[j] == false)
+                    count++;
+            }
+    }
     cout << count;
+
     return 0;
 }
